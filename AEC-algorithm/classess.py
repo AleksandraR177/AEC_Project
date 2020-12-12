761d105828cb74556856017c1cbda87cdead8cf3
kasia7625 <kasia7625>
2020-12-12 17:16:05 +0100

    Added new files

---
+++ AEC-algorithm/classess.py
@@ -0,0 +1,16 @@
+
+class SoundBase:
+
+    def __init__(self, samples, fs=44100, phrase=''):
+        self.samples = samples
+        self.fs = fs
+        self.phrase = phrase
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
+
+    def length(self):
+        return self.samples.size
